e9bb8a7af7a5c9b0ec6e10f901e793c10c4d1c8f
M. H. Nahib <mhnahib@gmail.com>
2023-12-28 22:50:54 +0600

    feat: added error handler

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,9 +1,9 @@
 import { Request, RequestHandler, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import {response} from "../utils";
+import { response } from "../utils";
 
 const homecontroller: RequestHandler = (req: Request, res: Response): void => {
-  response(res, StatusCodes.ACCEPTED, true, { say: "hello world" }, "Accepted");
+  response(res, StatusCodes.ACCEPTED, true, { info: "Simple api" }, "OK");
 };
 
 export default homecontroller;
